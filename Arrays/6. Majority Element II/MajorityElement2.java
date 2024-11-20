147b50e5d44e61420a0145949f4ed5c6ac012648
Hephzibah A <hephzibahantony11@gmail.com>
2024-11-20 20:36:18 +0530

    Majority Element 2

---
+++ Arrays/6. Majority Element II/MajorityElement2.java
@@ -0,0 +1,84 @@
+import java.util.* ;
+import java.io.*;
+
+public class MajorityElement2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String[] parts = s.split(" ");
+            int[] nums = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                nums[i] = Integer.parseInt(parts[i]);
+            }
+            Solution ob = new Solution();
+            List<Integer> ans = ob.findMajority(nums);
+
+            if (ans.size() == 0) {
+                System.out.println("[]");
+            } else {
+                for (int i : ans) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+            }
+        }
+        sc.close();
+    }
+}
+
+
+
+class Solution {
+    public List<Integer> findMajority(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Find potential candidates
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify the candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        // Step 3: Collect candidates with votes > n / 3
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+
+        // Return the result in increasing order
+        Collections.sort(result);
+        return result;
+    }
+}
